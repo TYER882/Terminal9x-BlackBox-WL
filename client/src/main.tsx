f7dc8a79eb046e7e4a80c00d28d88ae4162e6b58
Dem9x <dimasset9809@gmail.com>
2026-06-13 18:59:35 +0700

    vercel.json client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+window.addEventListener("unhandledrejection", (event) => {
+  const msg = String(event.reason?.message || "");
 
+  if (
+    msg.includes("Failed to fetch dynamically imported module") ||
+    msg.includes("ChunkLoadError")
+  ) {
+    window.location.reload();
+  }
+});
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
